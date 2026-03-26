140d8f066b4c6af97459b616768da0d5992396ea
CoYoTE <philippe@alcoy.co.uk>
2026-03-26 23:10:49 +0000

    Update d3_sort_algo.js

--- modules/d3_sort_algo.js
+++ modules/d3_sort_algo.js
@@ -1,1 +1,86 @@
+export { lowestExchangeSort, highestExchangeSort };
+
+export async function lowestExchangeSort() {
+  var j;
+
+  for (var i=1; i<noData; i++) {
+
+    if (startButton_status == "waiting") {
+      console.log("[lowestExchangeSort] Stop Buttom Clicked: Exiting");
+      break;
+    }
+
+    await highlightColourBar(i, colourScan, true);
+    value = dataset[i];
+    countReads++;
+    countAccesses++;
+    console.log("[lowestExchangeSort] i:" + i + " value[" + i + "]:" + value + " dataset:" + dataset);
+    j = i-1;
+    await highlightColourBar(i, colourScan, false);
+    while ((j>=0) && dataset[j]>value) {
+
+      if (startButton_status == "waiting") {
+        console.log("[lowestSelectionSort] Stop Buttom Clicked: Exiting");
+        break;
+      }
+
+      animationCount += 2;
+      swapBars(j, j+1);
+      await waitAnimationEnd();
+      
+      dataset[j+1] = dataset[j];
+      dataset[j] = value;
+      j--;
+
+      countReads       += 2;
+      countWrites      += 2;
+      countAccesses    += 3;
+      countSwaps       += 2;
+      countComparisons += 1;			
+    }
+    console.log("[lowestExchangeSort] After swap dataset:" + dataset);				
+  }
+}
+
+export async function highestExchangeSort() {
+  var j;
+
+  for (var i=noData-2; i>=0; i--) {
+
+    if (startButton_status == "waiting") {
+      console.log("[highestExchangeSort] Stop Buttom Clicked: Exiting");
+      break;
+    }
+
+    await highlightColourBar(i, colourScan, true);
+    value = dataset[i];
+    countReads++;
+    countAccesses++;
+    console.log("[highestExchangeSort] i:" + i + " value[" + i + "]:" + value + " dataset:" + dataset);
+    j = i+1;
+    await highlightColourBar(i, colourScan, false);
+    while ((j<noData) && dataset[j]<value) {
+
+      if (startButton_status == "waiting") {
+        console.log("[highestExchangeSort] Stop Buttom Clicked: Exiting");
+        break;
+      }
+
+      animationCount += 2;
+      swapBars(j, j-1);
+      await waitAnimationEnd();
+      
+      dataset[j-1] = dataset[j];
+      dataset[j] = value;
+      j++;
+
+      countReads       += 2;
+      countWrites      += 2;
+      countAccesses    += 3;
+      countSwaps       += 2;
+      countComparisons += 1;			
+    }
+    console.log("[highestExchangeSort] After swap dataset:" + dataset);				
+  }
+}
 
