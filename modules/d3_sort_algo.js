32de525bff94a9edba24d7c46e1e219d8b1ec9ed
CoYoTE <philippe@alcoy.co.uk>
2026-03-27 10:40:13 +0000

    Update d3_sort_algo.js

--- modules/d3_sort_algo.js
+++ modules/d3_sort_algo.js
@@ -1,6 +1,6 @@
 export { lowestExchangeSort, highestExchangeSort };
 
-import { highlightColourBar, swapBars, waitAnimationEnd } from "./d3_sort_animation.js";	
+import { highlightColourBar, swapBars, waitAnimationEnd } from "d3_sort_animation.js";	
 
 async function lowestExchangeSort() {
   var j, value;
